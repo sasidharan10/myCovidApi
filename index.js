a5e1839ead1bad89f526bf18de404555385d8068
sasidharan10 <ksasidharan98@gmail.com>
2023-07-13 19:24:43 +0530

    Set access control

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.use((req, res, next) => {
+    // Set the Access-Control-Allow-Origin header to allow requests from any origin
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Set the Access-Control-Allow-Methods header to allow the desired HTTP methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    // Set the Access-Control-Allow-Headers header to allow the desired headers
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // Pass the control to the next middleware
+    next();
+});
+
 const user_name = process.env.MONGO_USERNAME;
 const ps_word = process.env.MONGO_PS;
 
@@ -24,6 +38,6 @@ app.get("/", (req, res) => {
     });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    // console.log("http://localhost:3000/");
+app.listen(process.env.PORT || 5000, () => {
+    console.log("http://localhost:5000/");
 });
